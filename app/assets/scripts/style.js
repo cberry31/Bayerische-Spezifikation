85bff66e2c0df0865f6b5ed29786440e7e5610a7
colton.berry <colton.berry@wsu.edu>
2020-12-16 02:24:46 -0800

    Started the text for the compare page

--- app/assets/scripts/style.js
+++ app/assets/scripts/style.js
@@ -21,3 +21,19 @@ function setBGimage() {
     const imgStr = `url('./assets/images/${carName}Page.jpg')`;
     document.body.style.backgroundImage = imgStr;
 }
+
+function setCompareText(carName) {
+    const car = findCar(carName)[0];
+    document.getElementById("carName").innerHTML = printName(car).toString();
+    document.getElementById("carYear").innerHTML = printYear(car).toString();
+    document.getElementById("carExtrColor").innerHTML = printExteriorColor(car).toString();
+    document.getElementById("carInColor").innerHTML = printInteriorColor(car).toString();
+    document.getElementById("carBS").innerHTML = printBodyStyle(car).toString();
+    document.getElementById("carAVGmpg").innerHTML = printAvgMPG(car).toString();
+    document.getElementById("carTransmission").innerHTML = printTransmission(car).toString();
+    document.getElementById("carWB").innerHTML = printWheelBase(car).toString();
+    document.getElementById("carEngType").innerHTML = printEngineType(car).toString();
+    document.getElementById("carEngHP").innerHTML = printEngineHP(car).toString();
+    document.getElementById("carEngDis").innerHTML = printEngineDis(car).toString();
+    document.getElementById("carDesc").innerHTML = printDesc(car).toString();
+}
